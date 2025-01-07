05e94b6579e2db9e6bdcda90d525d6f4ada392d7
unomns <unomns@gmail.com>
2025-01-07 12:01:21 +0800

    init commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  webserver
+//
+//  Created by Murad Nurmukhamedov on 1/7/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
